8610c62294a4a07a53756a5652858c30e458bb4a
Cody Dennhardt <T00035975@mytru.ca>
2017-09-19 19:37:52 -0700

    Initial commit

---
+++ app/src/main/java/tru/comp2160_assignment3/MainActivity.java
@@ -0,0 +1,19 @@
+package tru.comp2160_assignment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+import android.content.Context;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
