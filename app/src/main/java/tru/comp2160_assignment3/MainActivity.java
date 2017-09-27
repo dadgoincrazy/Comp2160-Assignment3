ad8a32561081eb4ba13fb122741687bb6fa0a12c
T00035975 <dennhardtc12@gmail.com>
2017-09-26 20:59:39 -0700

    More UI

--- app/src/main/java/tru/comp2160_assignment3/MainActivity.java
+++ app/src/main/java/tru/comp2160_assignment3/MainActivity.java
@@ -17,5 +17,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void numeric_input(View v)
+    {
+        TextView input_string = (TextView) findViewById(R.id.input_string);
+        Button this_button = (Button) v;
 
+        String input = (String) this_button.getText();
+        String prev = (String) input_string.getText();
+        String new_text = prev;
+        String char_list = "+x/";
+        char last_char = prev.charAt(prev.length() -1);
+
+        if(prev.length() < 13)
+        {
+            if(char_list.contains(String.valueOf(last_char)))
+            {
+                if(char_list.contains(input))
+                {
+                    
+                }
+            }
+
+            new_text = prev + input;
+        }
+
+        input_string.setText(new_text);
+    }
+
+    public void del(View v)
+    {
+        TextView input_string = (TextView) findViewById(R.id.input_string);
+        String prev = (String) input_string.getText();
+
+        if(prev != null && prev.length() > 0)
+        {
+            prev = prev.substring(0, prev.length() - 1);
+        }
+
+        input_string.setText(prev);
+    }
+
+    public void clear(View v)
+    {
+        TextView input_string = (TextView) findViewById(R.id.input_string);
+        input_string.setText(null);
+    }
 }
